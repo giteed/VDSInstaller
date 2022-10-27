69e642673285d88da975f5e26f3623d1a67530ad
giteed <33794654+giteed@users.noreply.github.com>
2022-10-27 17:07:15 +0300

    Update pre.sh

--- bin/utility/pre.sh
+++ bin/utility/pre.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 
-
+	echo -e "  " 
 	# Установка rsync rsync-daemon git mc
 	(dnf -y install rsync rsync-daemon git mc) &>/devnull;
 	
 	
 
-function cp_old() # Функция копирования предыдущих .bash* файлов в /tmp/
+function cp_old() 
 {
-	# Переход в домашний каталог (root)
+	echo -e " # Функция копирования предыдущих .bash* файлов в /tmp/ " 
+	echo -e " # Переход в домашний каталог (root) " 
+	
 	(cd ~/) ; 
+	echo -e " # Сохранение старых версий  " 
+	
 	yes | cp -Rf .bash_ali_hosts /tmp/.bash_ali_hosts_old ;
 	yes | cp -Rf .bash_profile /tmp/.bash_profile_old ; 
 	yes | cp -Rf .bash_aliases /tmp/.bash_aliases_old ; 
 	yes | cp -Rf .bashrc /tmp/.bashrc_old 
 }
 	(cp_old) 2>/devnull ;
-	(source ~/.bashrc) ;
-# Создать каталог ~/root/bin если его нет 
+	
+	echo -e " # Создать каталог ~/root/bin если его нет " 	
 	(mkdir -p /root/bin) ; 
 
-cs ;
-	css ;
-	echo ok1 ; sleep 3 ;
-
-
-
+	 sleep 3 ;
+echo -e "  " 
 # Синхронизация локального репо /root/.VDSInstaller/ установщика с папкой ~/root
 (rsync -avp --progress --exclude '.git' --exclude '.DS_Store' /root/.VDSInstaller/ /root) ;
-
+echo -e "  " 
 # Перезагрузка ~/.bashrc
 (source ~/.bashrc) ;
+echo -e " # Установка VDSetup - переход к установке " 
 
-# Установка VDSetup
-echo ok2 ; sleep 3 ;
-	css ;
+
 	
 	
 exit 0 ;
