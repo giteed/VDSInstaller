ec3abc8226afd60b0cee46e8d807439320aa4dda
giteed <33794654+giteed@users.noreply.github.com>
2022-10-27 18:26:34 +0300

    Update pre.sh

--- bin/utility/pre.sh
+++ bin/utility/pre.sh
@@ -4,9 +4,9 @@
 	
 	echo ;
 	
-	echo -e "\n # Установка rsync rsync-daemon git mc " 
+	echo -e "\n # Установка rsync rsync-daemon git mc gh" 
 	
-	(dnf -y install rsync rsync-daemon git mc) &>/devnull;
+	(dnf -y install rsync rsync-daemon git mc gh) &>/dev/null ;
 	
 		echo ;
 	
@@ -29,9 +29,18 @@ function cp_old()
 	(mkdir -p /root/bin) ; 
 
 	 sleep 3 ;
-echo -e " # Синхронизация локального репо /root/.VDSInstaller/ установщика с папкой ~/root \n" 
-
+echo -e " #  GitHub Синхронизация локального репо /root/.VDSInstaller/ установщика с папкой ~/root \n" 
 (rsync -avp --exclude '.git' --exclude '.DS_Store' /root/.VDSInstaller/ /root) ;
+
+function gh_install()
+{
+	echo -e " $(black_U23A7 ) " ;
+	echo -e " $(blue_1      ) GitHub (gh) не установлен!" ;
+	echo -e " $(black_U23A9 ) " ;
+	/root/bin/utility/github.sh ;
+}
+(gh || gh_install)
+
 
 echo -e "\n # Перезагрузка ~/.bashrc " ;
 (source ~/.bashrc) ;
