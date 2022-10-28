6e5e6921dc736ac40a8c922e2e2ba67b1a62a742
giteed <33794654+giteed@users.noreply.github.com>
2022-10-28 23:05:08 +0300

    Update installVDSetup.sh

--- bin/utility/installVDSetup.sh
+++ bin/utility/installVDSetup.sh
@@ -39,11 +39,11 @@ echo -e " $(black_U23A9    ) \n" ;
 
 press_anykey ; css ; sleep 1 ;
 
-echo -e "${black}/Documents${NC}/GitHub/VDSetup-Script-Installer/bin/utility/installVDSetup.sh"
+
 # Установка VDSetup
  echo -e "\n  	$(green_arrow) Installing \"VDSetup\" scripts ... : $(green_tick) ${GREEN}In progress ...${NC}" ;
 	( /root/bin/utility/gh-Sync-Script.sh ) && echo -e "\n	$(green_arrow) Install vdsetup .... : $(green_tick) ${GREEN}Done${NC}! " || echo -e "${RED}ERROR\n${NC}" ;
-echo -e "${black}/Documents${NC}/GitHub/VDSetup-Script-Installer/bin/utility/installVDSetup.sh"
+
 	echo -e "\n $(black_U23A7 ) " ;
 	echo -e " $(ellow_1     ) $(green_arrow) Reload ~/.bashrc and Run vdsetup" ;
 	echo -e " $(white_1     ) ${RED}#${NC} source ~/.bashrc ${NC}" ;
