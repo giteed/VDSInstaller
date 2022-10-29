f9898161b5408e339de6b98ae460bdf9cf28c023
giteed <33794654+giteed@users.noreply.github.com>
2022-10-30 00:24:31 +0300

    Update installVDSetup.sh

--- bin/utility/installVDSetup.sh
+++ bin/utility/installVDSetup.sh
@@ -41,7 +41,7 @@ echo -e " $(red_1          ) ${RED}Do not continue${NC} if the ${ELLOW}/root/bin
 echo -e " $(red_1          ) contains the files you need${RED}!${NC}" ;
 echo -e " $(black_U23A9    ) \n" ;
 
-press_anykey ; # css ; 
+#press_anykey ; # css ; 
 
 
 # Установка VDSetup
