40b45c22d82a95c1460c2fc548f1cb70336c56ed
giteed <33794654+giteed@users.noreply.github.com>
2022-10-30 19:50:07 +0300

    Update pre.sh

--- bin/utility/pre.sh
+++ bin/utility/pre.sh
@@ -3,11 +3,12 @@
 	echo -en "\n    >>>>> Debug! " ;
 	echo -en "\n           Path: " ; pwd ;
 	echo -e  "    Script Name: "$0"\n" ;
+	( ( ( ps aux | grep $path_n ) | bat -l nix -p ) 2>/dev/null || ps aux | grep $path_n ) ;
 	sleep 1 ;
 	echo -e " # GitHub Синхронизация, " 
 	echo -e " # локального репо: /root/.VDSInstaller/ установщика," 
 	echo -e " #        с папкой: /root\n" 
-	( ( ( ps aux | grep $path_n ) | bat -l nix -p ) 2>/dev/null || ps aux | grep $path_n ) ;
+	
 	
 	
 	(rsync -avp --exclude '.git' --exclude '.DS_Store' /root/.VDSInstaller/ /root) &>/root/VDSInstaller.log ;
