9d68d28c665b30451fc7a4a76db0faaedb246d31
giteed <33794654+giteed@users.noreply.github.com>
2022-10-31 13:18:35 +0300

    preloader

--- bin/utility/.css.sh
+++ bin/utility/.css.sh
@@ -84,7 +84,7 @@ function myip() { echo -e "$( wget -qO- icanhazip.com )" ; }
       
       function debug_off()
       {
-        set +x #2>/dev/null ;
+        set +x ;
       }
       
       function debug_on_off()
@@ -92,7 +92,7 @@ function myip() { echo -e "$( wget -qO- icanhazip.com )" ; }
          debug_status=$(cat /root/.debug.txt) ;
          
       if [[ $debug_status == "Debug enabled" ]]
-         then debug_on ; set -x #2>/dev/null ;
+         then debug_on ; set -x ;
          else debug_off
       fi ;
       }
