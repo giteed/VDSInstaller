09905da1338028dca04a9f95ddd81a8e672635af
giteed <33794654+giteed@users.noreply.github.com>
2022-11-02 18:48:33 +0300

    Update preloader.sh

--- bin/utility/preloader.sh
+++ bin/utility/preloader.sh
@@ -1,14 +1,111 @@
 
 #!/bin/bash
 
+   #------------------------------------
+	  # Информация о версии скрипта VDSetup
+	  #------------------------------------ 
+	  
+	function ver() 
+	{ 
+	   source ~/.bashrc ; #css ; 
+	   echo -e "\n $(Version_vdsetup)" ;
+	}
+
    
+   #------------------------------------
+	  # Cтатус "Debug"
+	  #------------------------------------ 
+	  
+	function ds() 
+	{ 
+	   #css ;
+	   source ~/.bashrc ; #css ; 
+	   echo -e "\n ${msg_debug_stat}" ;
+	}
+
+
+function test100()
+{
+   echo -e "$$$$" 
+}
+
+
+
+   #------------------------------------
+	  # Version_vdsetup
+	  #------------------------------------ 
+	  
+	function Version_vdsetup()
+	  {
+		 echo -e "Ver: GH-1.0.0.55"  #| bat -l nix ) #|| $( echo -e "${RED}Ver${NC}: GH-1.0.0${NC}" ) 
+	  }
+
+   debug_stat=$(cat /root/.debug.txt) ;
+   
+   #------------------------------------
+	  # script_name debug
+	  #------------------------------------   
+	  
+	function debug()
+	  {
+		 
+		 function debug_1_on()
+		 {
+			path_n=$0
+			echo -e "debug status is $debug_stat" ;
+			
+			echo -e "        $(black_U23A7    ) " ;
+			echo -e "       $(red_star)$(red_1          ) ${NC}!${NC}${BLACK}#${RED} ---------------${BLACK}Debug${RED}--------------- ${BLACK}#${NC}!" ;
+			echo -en "       $(red_star)$(ellow_1       )  ${BLACK}Path ......${NC}: " ; echo -e "${NC}$(pwd)" ;
+			echo -e "       $(red_star)$(ellow_1        )  ${BLACK}Script Name${NC}:${GREEN} "$0" ${NC}" ;
+			echo -e "       $(red_star)$(ellow_1        )  ${BLACK}Date ......${NC}: "$(date)" ${NC}" ;
+			echo -e "       $(red_star)$(ellow_1        ) " ; ( ( ( ps aux | grep $path_n ) | bat -l nix -p ) 2>/dev/null || ps aux | grep $path_n ) ;
+			echo -e "       $(red_star)$(ellow_1        ) " ;
+			echo -e "        $(black_U23A9    ) \n" ;
+		 }
+		 
+		 function debug_0_off()
+		 {
+		  echo -e "debug status is $debug_stat" ;
+		 }
+		 
+		 function debug_check_status()
+		 {
+			
+			
+		 if [[ $debug_stat == "1" ]]
+			then debug_1_on ;
+			else debug_0_off
+		 fi ;
+		 }
+		 
+		 debug_check_status ;
+		 
+	  }
+   
+   #------------------------------------
+	  # script_name
+	  #------------------------------------  
+	  
+	function script_name() { (debug ;) 2>/dev/null || echo no_debug ; }
+   
+
    #------------------------------------
    # баннер
    #------------------------------------
    
+   if [[ $debug_stat == '1' ]] 
+	  then msg_debug_stat=$(echo -e "${GREEN}Debug enabled${NC}") ;
+	  else msg_debug_stat=$(echo -e "${RED}Debug disabled${NC}") ;
+   fi ;
+   
 function css() 
    { 
-	  echo -en "$( clear && source ~/.bashrc && hip )\n ${gray}(For update type: vdsetup -sync)${NC}\n ${RED}-${ellow}=---${ELLOW}=${ellow}-${GREEN}-${green}-${NC}-------------------------------------------------------------------------\n ${NC}$(green_tick) VDSetup scripts $(Version_vdsetup)" ; echo -e "${BLACK}   ($(cat ~/.debug.txt))${NC}"  
+	 
+	  echo -en "$( clear && source ~/.bashrc && hip )\n ${gray}(For update type: vdsetup -sync)${NC}\n ${RED}-${ellow}=---${ELLOW}=${ellow}-${GREEN}-${green}-${NC}-------------------------------------------------------------------------\n ${NC}$(green_tick) VDSetup scripts $(Version_vdsetup)" ;  echo -e " ${msg_debug_stat}" ;
+	  
+	   
+	
    }
    
    #-----------------------------------
@@ -24,7 +121,7 @@ function hip() # host/ip
    }
 
    #------------------------------------
-   # баннер
+   # шаблон
    #------------------------------------
    
    #-----------------------------------
@@ -41,71 +138,12 @@ function hip() # host/ip
 	  function im() { whoami ; } ;
 	  
 
-
-   #------------------------------------
-   # Version_vdsetup
-   #------------------------------------ 
-   
-function Version_vdsetup()
-   {
-	  echo -e "Ver: GH-1.0.0"  #| bat -l nix ) #|| $( echo -e "${RED}Ver${NC}: GH-1.0.0${NC}" )
-	  
-   }
-
    #------------------------------------
    # my ip
    #------------------------------------ 
 
 function myip() { echo -e "$( wget -qO- icanhazip.com )" ; }
 
-
-
-   #------------------------------------
-   # script_name debug
-   #------------------------------------   
-   
- function debug()
-   {
-	  
-	  function debug_on()
-	  {
-		 path_n=$0
-		 
-		 echo -e "        $(black_U23A7    ) " ;
-		 echo -e "       $(red_star)$(red_1          ) ${NC}!${NC}${BLACK}#${RED} ---------------${BLACK}Debug${RED}--------------- ${BLACK}#${NC}!" ;
-		 echo -en "       $(red_star)$(ellow_1       )  ${BLACK}Path ......${NC}: " ; echo -e "${NC}$(pwd)" ;
-		 echo -e "       $(red_star)$(ellow_1        )  ${BLACK}Script Name${NC}:${GREEN} "$0" ${NC}" ;
-		 echo -e "       $(red_star)$(ellow_1        )  ${BLACK}Date ......${NC}: "$(date)" ${NC}" ;
-		 echo -e "       $(red_star)$(ellow_1        ) " ; ( ( ( ps aux | grep $path_n ) | bat -l nix -p ) 2>/dev/null || ps aux | grep $path_n ) ;
-		 echo -e "       $(red_star)$(ellow_1        ) " ;
-		 echo -e "        $(black_U23A9    ) \n" ;
-	  }
-	  
-	  function debug_off()
-	  {
-		 2>/dev/null ;
-	  }
-	  
-	  function debug_on_off()
-	  {
-		 debug_status=$(cat /root/.debug.txt) ;
-		 
-	  if [[ $debug_status == "Debug enabled" ]]
-		 then debug_on 
-		 else debug_off
-	  fi ;
-	  }
-	  
-	  debug_on_off ;
-	  
-   }
-
-   #------------------------------------
-   # script_name
-   #------------------------------------  
-   
-function script_name() { (debug ;) 2>/dev/null || echo no_debug ; }
-	
    #-----------------------------------
    # Таблица символов Юникода
    # https://unicode-table.com/ru/ 
@@ -181,6 +219,7 @@ function green_star37()
    {
 	  echo -e "$(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) $(green_star) " ;
    }
+
 
 	echo ;
 	script_name ;
