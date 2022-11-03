82ee73e06ae9869c8ddde69d57d57c54f590ffcb
giteed <33794654+giteed@users.noreply.github.com>
2022-11-03 13:26:15 +0300

    Update installVDSetup.sh

--- bin/utility/installVDSetup.sh
+++ bin/utility/installVDSetup.sh
@@ -26,27 +26,30 @@ script_name ;
 # всех скриптов из GitHub Repo.
 echo ;
 
+# Welcome to VDSetup Installer
+function welcome_to_installer()
+{
+		(( echo -e "   Welcome to VDSetup Installer " ; echo -e "   for CentOS\n") | bat -l nix -p 2>/dev/null ) || ( echo -e "   Welcome to VDSetup Installer " ; echo -e "   for CentOS\n") ;
+		
+		echo -e " $(black_U23A7    ) " ;
+		echo -e " $(blue_1         ) Installing the latest version" ;
+		echo -e " $(green_1        ) of all scripts from GitHub Repo." ;
+		echo -e " $(black_U23A9    )\n" ;
+		echo -e " $(black_U23A7    ) " ;
+		echo -e " $(ellow_1        ) Installation path: ${NC}/root/bin" ;
+		echo -e " $(red_1          ) ${RED}All contents of the${NC}: ${ELLOW}/root/bin ${RED}folder" ;
+		echo -e " $(red_1          ) ${RED}will be deleted before installation${NC}!" ;
+		echo -e " $(red_1          ) ${RED}Do not continue${NC} if the ${ELLOW}/root/bin ${NC}folder" ;
+		echo -e " $(red_1          ) contains the files you need${RED}!${NC}" ;
+		echo -e " $(black_U23A9    ) \n" ;
+}
 
-(( echo -e "   Welcome to VDSetup Installer " ; echo -e "   for CentOS\n") | bat -l nix -p 2>/dev/null ) || ( echo -e "   Welcome to VDSetup Installer " ; echo -e "   for CentOS\n") ;
-
-echo -e " $(black_U23A7    ) " ;
-echo -e " $(blue_1         ) Installing the latest version" ;
-echo -e " $(green_1        ) of all scripts from GitHub Repo." ;
-echo -e " $(black_U23A9    )\n" ;
-echo -e " $(black_U23A7    ) " ;
-echo -e " $(ellow_1        ) Installation path: ${NC}/root/bin" ;
-echo -e " $(red_1          ) ${RED}All contents of the${NC}: ${ELLOW}/root/bin ${RED}folder" ;
-echo -e " $(red_1          ) ${RED}will be deleted before installation${NC}!" ;
-echo -e " $(red_1          ) ${RED}Do not continue${NC} if the ${ELLOW}/root/bin ${NC}folder" ;
-echo -e " $(red_1          ) contains the files you need${RED}!${NC}" ;
-echo -e " $(black_U23A9    ) \n" ;
-
-#press_anykey ; echo -e "\n" ;
-#press_enter_to_continue_or_ESC_to_cancel ; echo -e "\n" ; # css ; 
-press_enter_to_continue_or_any_key_to_cancel ;
 
 # Установка VDSetup
- echo -e "  	$(green_arrow) Installing \"VDSetup\" scripts ... : $(green_tick) ${GREEN}In progress ...${NC}\n" ;
+function Installing_VDSetup_scripts()
+{
+	
+    echo -e "  	$(green_arrow) Installing \"VDSetup\" scripts ... : $(green_tick) ${GREEN}In progress ...${NC}\n" ;
 	( /root/bin/utility/gh-Sync-Script.sh ) && echo -e "\n	$(green_arrow) Install vdsetup .... : $(green_tick) ${GREEN}Done${NC}! " || echo -e "${RED}ERROR\n${NC}" ;
 
 	echo -e "\n $(black_U23A7 ) " ;
@@ -55,6 +58,11 @@ press_enter_to_continue_or_any_key_to_cancel ;
 	echo -e " $(white_1     ) ${RED}#${NC} vdsetup ${NC}" ;
 	
 	echo -e " $(black_U23A9 ) " ;
+}
+
+welcome_to_installer ;
+press_enter_to_continue_or_any_key_to_cancel ;
+Installing_VDSetup_scripts ;
 	
 exit 0 ;
 
