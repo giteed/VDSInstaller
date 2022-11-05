a8695b5b523045733b4a08f5948a9e9cd5b6ecc4
giteed <33794654+giteed@users.noreply.github.com>
2022-11-05 04:03:02 +0300

    Update installVDSetup.sh

--- bin/utility/installVDSetup.sh
+++ bin/utility/installVDSetup.sh
@@ -59,7 +59,7 @@ function Installing_VDSetup_scripts()
 	echo -e " $(ellow_1     ) $(green_arrow) Reload ~/.bashrc and Run vdsetup" ;
 	echo -e " $(white_1     ) ${RED}#${NC} source ~/.bashrc ${NC}" ;
 	echo -e " $(white_1     ) ${RED}#${NC} vdsetup ${NC}" ;
-	echo -en "${RED}" ; Version_vdsetup ;
+	echo -e "${RED}" ; Version_vdsetup ;
 	echo -e " $(black_U23A9 ) " ;
 }
 
